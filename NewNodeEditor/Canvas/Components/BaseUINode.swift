a98f8dd78621d9514569528648d77bb3f67000b8
Matt Novoselov <matveynovoselov@gmail.com>
2024-03-20 16:47:52 +0100

    Added scaling up and shadow to nodes, while dragging

--- NewNodeEditor/Canvas/Components/BaseUINode.swift
+++ NewNodeEditor/Canvas/Components/BaseUINode.swift
@@ -11,21 +11,37 @@ struct BaseUINode: View {
     @Binding var nodePosition: CGPoint
     var customOverlay: AnyView?
     
+    @State private var scale: CGFloat = 1.0
+    @State private var shadowOpacity: CGFloat = 0.0
+    
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 25.0)
                 .stroke(Color.black, lineWidth: 5) // Adjust the color and line width as needed
                 .background(RoundedRectangle(cornerRadius: 25.0).foregroundColor(.white))
+                .shadow(color: .black.opacity(shadowOpacity), radius: 15)
             
             customOverlay
                 .padding()
         }
         .frame(width: 200, height: 200)
         .position(nodePosition)
+        .scaleEffect(scale)
         .gesture(
             DragGesture()
                 .onChanged { value in
                     nodePosition = value.location
+                    
+                    withAnimation{
+                        scale = 1.1
+                        shadowOpacity = 0.2
+                    }
+                }
+                .onEnded{ _ in 
+                    withAnimation{
+                        scale = 1.0
+                        shadowOpacity = 0.0
+                    }
                 }
         )
     }
